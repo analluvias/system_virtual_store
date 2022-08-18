6820172be67afd07a2048528f7b70b7b23b7097d
Ana Lívia Meira <analiviameira@gmail.com>
2022-08-18 11:44:50 -0300

    Creating class ProductEspecification

---
+++ src/entities/ProductEspecification.java
@@ -0,0 +1,43 @@
+package entities;
+
+public class ProductEspecification {
+    private String description;
+    private Double price;
+
+    private Integer id;
+
+    public ProductEspecification(String description, Double price, Integer id) {
+        this.description = description;
+        this.price = price;
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "ProductEspecification{" +
+                "description='" + description + '\'' +
+                ", price=" + price +
+                ", id=" + id +
+                '}';
+    }
+}
