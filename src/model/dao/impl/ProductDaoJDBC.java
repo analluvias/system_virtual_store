596074458c2e3a75e521a667f8b16afa7b1677e4
Ana Lívia Meira <analiviameira@gmail.com>
2022-08-25 10:25:54 -0300

    adding functionality to the ProductDaoJDBC

--- src/model/dao/impl/ProductDaoJDBC.java
+++ src/model/dao/impl/ProductDaoJDBC.java
@@ -4,10 +4,15 @@ import db.DB;
 import db.DbException;
 import model.dao.ProductDao;
 import model.dao.ProductEspecificationDao;
+import model.entities.Client;
 import model.entities.Product;
+import model.entities.ProductEspecification;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ProductDaoJDBC implements ProductDao {
 
@@ -54,22 +59,123 @@ public class ProductDaoJDBC implements ProductDao {
     }
 
     @Override
-    public void update(Product obj) {
-
-    }
-
-    @Override
     public void deleteById(Integer id) {
+        PreparedStatement st = null;
 
+        try {
+            st = conn.prepareStatement("DELETE FROM PRODUCT WHERE PRODUCTID_ = ?");
+
+            st.setInt(1, id);
+
+            st.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }
+        finally {
+            DB.closeStatement(st);
+        }
     }
 
     @Override
     public Product findById(Integer id, ProductEspecificationDao productEspecificationDao) {
-        return null;
+        PreparedStatement st = null;
+        ResultSet rs = null;
+
+        try {
+            st = conn.prepareStatement("SELECT PRODUCT.*, PRODUCTESPECIFICATION.PRODUCTESPECIFICATIONID_ " +
+                    "FROM PRODUCT " +
+                    "INNER JOIN PRODUCTESPECIFICATION " +
+                    "ON PRODUCT.PRODUCTESPECIFICATIONID_ = PRODUCTESPECIFICATION.PRODUCTESPECIFICATIONID_ " +
+                    "WHERE PRODUCT.PRODUCTID_ = ?");
+
+            st.setInt(1, id);
+
+            rs = st.executeQuery();
+
+            //testando se veio algum registro
+            if (rs.next()){
+
+                //criando o departamento no java
+                ProductEspecification productEspecification = productEspecificationDao
+                        .findById(rs.getInt("PRODUCTESPECIFICATIONID_"));
+
+                //instanciando o vendedor no java
+                Product product = instantiateProduct(rs, productEspecification);
+
+                return product;
+            }
+
+            //se não veio registro
+            return null;
+        }
+        catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }
+        finally {
+            //fechando o enviador de clausulas
+            DB.closeStatement(st);
+
+            //fechando o recebedor de resultados
+            DB.closeResultSet(rs);
+        }
     }
 
     @Override
     public List<Product> findAll(ProductEspecificationDao productEspecificationDao) {
-        return null;
+
+
+        PreparedStatement st = null;
+        ResultSet rs = null;
+
+        try {
+            st = conn.prepareStatement("SELECT PRODUCT.*, PRODUCTESPECIFICATION.PRODUCTESPECIFICATIONID_ " +
+                    "FROM PRODUCT " +
+                    "INNER JOIN PRODUCTESPECIFICATION " +
+                    "ON PRODUCT.PRODUCTESPECIFICATIONID_ = PRODUCTESPECIFICATION.PRODUCTESPECIFICATIONID_ ");
+
+            rs = st.executeQuery();
+
+            List<Product> list = new ArrayList<>();
+            Map<Integer, ProductEspecification> map = new HashMap<>();
+
+            //enquanto houver valores no ResultSet
+            while (rs.next()){
+
+                ProductEspecification productEspecification = map.get(rs.getInt("PRODUCTESPECIFICATIONID_"));
+
+                if (productEspecification == null){
+                    productEspecification = productEspecificationDao.findById(rs.getInt("PRODUCTESPECIFICATIONID_"));
+
+                    map.put(rs.getInt("PRODUCTESPECIFICATIONID_"), productEspecification);
+                }
+
+                Product product = instantiateProduct(rs, productEspecification);
+                list.add(product);
+            }
+
+            return list;
+
+        }
+        catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }
+        finally {
+            DB.closeStatement(st);
+
+            DB.closeResultSet(rs);
+        }
+    }
+
+    private Product instantiateProduct(ResultSet rs, ProductEspecification productEspecification) throws SQLException {
+
+        Product product = new Product();
+
+        product.setId(rs.getInt("PRODUCTID_"));
+
+        product.setProductEspecification(productEspecification);
+
+        return product;
+
     }
 }
